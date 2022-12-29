7c7a8c9c93fe9180071474bf89687c12e0ed19d6
Rajil <azizrajil03@gmail.com>
2022-12-29 22:02:25 +0530

    update

--- frontend/src/components/manageshow.js
+++ frontend/src/components/manageshow.js
@@ -4,34 +4,24 @@ import { useState, useEffect } from 'react';
 const ShowManager = () => {
 
     const [userList, setUserList] = useState([]);
-    const [loading, setLoading] = useState(false);
 
     // for holding user data to update
-    // const [userFormData, setUserFormData] = useState(null);
+    const [userFormData, setUserFormData] = useState(null);
     
     // to show or hide update form
-    // const [showForm, setShowForm] = useState(false);
+    const [showForm, setShowForm] = useState(false);
 
     // this will fetch user data from backend
     const getDataFromBackend = async () => {
-        setLoading(true);
         const response = await fetch('http://localhost:5000/shows/getall');
         const data = await response.json();
         console.log(data);
         setUserList(data);
-        setLoading(false);
     }
 
-
-
-    // Effect Hook
-    // this will tell react what to do when component opens
-    useEffect(() => {
-      getDataFromBackend();
-    }, []);
     const deleteUser = async (id) => { 
         console.log(id);
-        const response = await fetch('http://localhost:5000/shows/delete/'+id, {method : 'DELETE',})
+        const response = await fetch('http://localhost:5000/shows/delete/'+id, {method : 'DELETE'})
         if(response.status === 200){
             console.log('user deleted');
             // toast.success('User Deleted 😎');
@@ -39,17 +29,13 @@ const ShowManager = () => {
         }
      }
 
+    // Effect Hook
+    // this will tell react what to do when component opens
+    useEffect(() => {
+      getDataFromBackend();
+    }, []);
+
     const displayUsers = () => {
-        if (loading) {
-            return (
-              <div className="text-center">
-                <div className="spinner-border" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div>
-              </div>
-            );
-          } 
-          else{
         return <table className='table bg-white'>
             <thead>
                 <tr>
@@ -64,16 +50,16 @@ const ShowManager = () => {
             </thead>
             <tbody>
                 {
-                    userList.map( (users) => (
+                    userList.map( (user) => (
                         <tr>
-                            <td>{users._id}</td>
-                            <td>{users.title}</td>
-                            <td>{users.views}</td>
-                            <td>{users.reviews}</td>
-                            <td>{users.publisher}</td>
-                            <td>{new Date(users.createdAt).toLocaleDateString()}</td>
+                            <td>{user._id}</td>
+                            <td>{user.title}</td>
+                            <td>{user.views}</td>
+                            <td>{user.reviews}</td>
+                            <td>{user.publisher}</td>
+                            <td>{new Date(user.createdAt).toLocaleDateString()}</td>
                             <td>
-                                <button className='btn btn-outline-danger' onClick={ (e) => deleteUser(users._id) }>
+                                <button className='btn btn-outline-danger' onClick={ () => { deleteUser(user._id) } }>
                                     <i className="fas fa-trash"></i>
                                 </button>
                             </td>
@@ -84,7 +70,7 @@ const ShowManager = () => {
             </tbody>
         </table>
     }
-    }
+
   return (
     <div className='container-fluid'>
         <h1 className='text-center'>Show Manager</h1>
